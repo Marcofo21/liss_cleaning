1f7dddcd3cc990117d52996603aa9b178bb1af18
Marcofo21 <marco.forti@uni-bonn.de>
2025-01-29 19:11:14 +0100

    Automate data_dictionary.

--- src/liss_cleaning/data_dictionary.py
+++ src/liss_cleaning/data_dictionary.py
@@ -1,4 +1,6 @@
-from liss_cleaning.config import BLD_CLEANED_DATA
+import os
+
+from liss_cleaning.config import BLD_CLEANED_DATA, SRC
 
 
 def _get_source_product_files(dataset_name):
@@ -22,6 +24,19 @@ def _make_product_path(time_identifier, dataset_name):
     return BLD_CLEANED_DATA / dataset_name / f"{dataset_name}_{time_identifier}.arrow"
 
 
+def _get_dataset_names():
+    """Get the names of the datasets."""
+    file_names = os.listdir(
+        SRC / "data_cleaning" / "make_normalized_datasets" / "specific_cleaners"
+    )
+    file_names = [name for name in file_names if name.endswith(".py")]
+    file_names = [name for name in file_names if name != "__init__.py"]
+    file_names = [name.replace(".py", "") for name in file_names]
+    file_names = [name.replace("_cleaner", "") for name in file_names]
+    return file_names
+
+
 data_dictionary = {
-    "economic_situation_assets": _get_source_product_files("economic_situation_assets"),
+    dataset_name: _get_source_product_files(dataset_name)
+    for dataset_name in _get_dataset_names()
 }
