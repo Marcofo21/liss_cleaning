20771ec34bc19a48851711c9a88bd4892e7e7401
Marco Forti <marco.forti@uni-bonn.de>
2025-02-06 11:08:46 +0100

    First attempt at including DataCatalogs.

--- src/liss_cleaning/data_cleaning/make_normalized_datasets/task_clean_datasets.py
+++ src/liss_cleaning/data_cleaning/make_normalized_datasets/task_clean_datasets.py
@@ -1,14 +1,13 @@
 """Task to make individual cleaned datasets."""
 
 import importlib
-from pathlib import Path
+from typing import Annotated
 
 import pandas as pd
-from pytask import task
+from pytask import DataCatalog, PickleNode, Product, task
 
-from liss_cleaning.config import BLD, SRC
-from liss_cleaning.data_dictionary import data_dictionary
-from liss_cleaning.helper_modules.load_save import load_data, save_data
+from liss_cleaning.config import SRC_DATA
+from liss_cleaning.helper_modules.load_save import load_data
 
 
 def _get_cleaning_function_from_dataset_module(dataset_name):
@@ -19,40 +18,74 @@ def _get_cleaning_function_from_dataset_module(dataset_name):
     return module.clean_dataset
 
 
-to_produce = data_dictionary.keys()
+## No advantage to using catalogs here tbh; only difference between this and the
+# previous
+## is that intermediate gets stored in pickle; which was achievable anyways.
+## I understand the point in ML projects with insanely large dataframes, but this...
+data_catalogs_individual_datasets = {
+    "ambiguous_beliefs": {
+        "xxx-ambiguous-beliefs/wave-1/L_gaudecker2018_1_6p.dta": DataCatalog(
+            name="ambiguous_beliefs_wave_1"
+        ),
+        "xxx-ambiguous-beliefs/wave-2/L_gaudecker2018_2_6p.dta": DataCatalog(
+            name="ambiguous_beliefs_wave_2"
+        ),
+        "xxx-ambiguous-beliefs/wave-3/L_gaudecker2019_3_6p.dta": DataCatalog(
+            name="ambiguous_beliefs_wave_3"
+        ),
+        "xxx-ambiguous-beliefs/wave-4/L_gaudecker2019_4_6p.dta": DataCatalog(
+            name="ambiguous_beliefs_wave_4"
+        ),
+        "xxx-ambiguous-beliefs/wave-5/L_gaudecker2020_5_6p.dta": DataCatalog(
+            name="ambiguous_beliefs_wave_5"
+        ),
+        "xxx-ambiguous-beliefs/wave-6/L_gaudecker2020_6_6p.dta": DataCatalog(
+            name="ambiguous_beliefs_wave_6"
+        ),
+        "xxx-ambiguous-beliefs/wave-7/L_gaudecker2021_7_6p.dta": DataCatalog(
+            name="ambiguous_beliefs_wave_7"
+        ),
+    },
+}
+# `data_catalogs_stacked_datasets` is a dictionary that contains information about
+# stacked
+# datasets. In this specific code snippet, it is used to store a single DataCatalog
+# object
+# for the stacked dataset of "ambiguous_beliefs". This DataCatalog object is named
+# "ambiguous_beliefs_stacked".
+data_catalogs_stacked_datasets = {
+    "ambiguous_beliefs": DataCatalog(name="ambiguous_beliefs_stacked"),
+}
 
-for new_dataset in to_produce:
-    for source_file in data_dictionary[new_dataset]:
-        path_to_produce = data_dictionary[new_dataset][source_file]
-        cleaning_func = _get_cleaning_function_from_dataset_module(new_dataset)
+for new_dataset, catalogs in data_catalogs_individual_datasets.items():
+    for wave_data_path, catalog in catalogs.items():
+        func = _get_cleaning_function_from_dataset_module(new_dataset)
 
-        @task(id=f"clean_{new_dataset}_{source_file.stem}")
-        def task_clean_dataset(
-            source_path: Path = source_file,
-            produces=path_to_produce,
-            cleaning_func=cleaning_func,
-            cleaning_script=SRC
-            / "data_cleaning"
-            / "make_normalized_datasets"
-            / "specific_cleaners"
-            / f"{new_dataset}_cleaner.py",
-        ):
-            """Clean the dataset."""
-            raw = load_data(source_path)
-            cleaned = cleaning_func(raw, source_path)
-            save_data(cleaned, produces)
+        @task(id=f"clean_{new_dataset}_dataset_{wave_data_path}")
+        def task_clean_one_dataset(
+            path=SRC_DATA / wave_data_path,
+            node: Annotated[PickleNode, Product] = data_catalogs_individual_datasets[
+                new_dataset
+            ][wave_data_path],
+            function=func,
+        ) -> Annotated[pd.DataFrame, catalog["cleaned"]]:
+            raw = load_data(path)
+            cleaned = function(raw, path)
+            return cleaned
+            # save doesn't store the pickle file??? what is this
+            # it's just unnecessary complications (product files inside .pytask????)
+            # EVERYONE understands paths; no one will have time to understand a class
+            # so unclearly defined
 
-
-for new_dataset in to_produce:
-    dep_dictionary = data_dictionary[new_dataset]
-
-    @task(id=f"merge_waves_{new_dataset}")
-    def task_merge_survey(
-        data_paths=dep_dictionary,
-        produces=BLD / "merged_waves" / f"{new_dataset}.arrow",
-    ):
-        """Merge the cleaned datasets."""
-        df = pd.DataFrame()
-        for dataset in data_paths.values():
-            df = pd.concat([df, load_data(dataset)], axis=0)
-        save_data(df, produces)
+    # data catalogs don't get updated between tasks LOL killer feature
+    @task(id=f"stack_{new_dataset}_datasets")
+    def task_stack_datasets(
+        dfs=[
+            data_catalogs_individual_datasets[new_dataset][wave_data_path]["cleaned"]
+            for wave_data_path in catalogs
+        ],
+    ) -> Annotated[
+        pd.DataFrame, data_catalogs_stacked_datasets[new_dataset]["cleaned"]
+    ]:
+        data = pd.concat(dfs)
+        return data
