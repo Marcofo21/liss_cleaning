3b78b549312db0d2e561f0b8b4aa034c6f6208d1
Marco Forti <marco.forti@uni-bonn.de>
2025-04-01 15:30:12 +0200

    Reset to previous commit for bug.

--- src/liss_cleaning/make_final_datasets/cleaners/matching_probabilities.py
+++ src/liss_cleaning/make_final_datasets/cleaners/matching_probabilities.py
@@ -12,15 +12,14 @@ dependencies_time_index = {
 def clean_dataset(raw):
     df = pd.DataFrame()
     options_ambiguous = {
-        1: "e>1000",
-        2: "e>1100",
-        3: "e<950",
-        4: "950<e<1100",
-        5: "e>1100c",
-        6: "e<950c",
-        7: "950<e<1100c",
+        1: "e0",
+        2: "e1",
+        3: "e2",
+        4: "e3",
+        5: "e1c",
+        6: "e2c",
+        7: "e3c",
     }
-    raw = raw.loc[_get_discared_observations_mask(raw)]
 
     df["personal_id"] = raw["personal_id"]
     for option in options_ambiguous.values():
@@ -29,32 +28,14 @@ def clean_dataset(raw):
             lambda x, opt=option: _get_interval(x, opt), axis=1
         )
     df["wave"] = raw["wave"]
+    df = df.groupby(["personal_id", "wave"]).filter(_check_answered_all_questions)
     filtered_df = df.groupby("personal_id").filter(_check_eligible)
     return filtered_df
 
 
-def _get_discared_observations_mask(survey_data):
-    """Get a mask for observations that should be discarded."""
-    time_used = survey_data["start_time"] - survey_data["end_time"]
-    time_mask = time_used > time_used.quantile(0.15)
-    choice_mask = survey_data.filter(regex=r"^choice_").apply(
-        lambda x: x.nunique() > 1,  # noqa: PD101
-        axis=1,
-    )
-    return time_mask & choice_mask
-
-
 def _check_answered_all_questions(row):
     """Return True if all questions in a row are answered, False otherwise."""
-    question_cols = [
-        "mp_e>1000",
-        "mp_e>1100",
-        "mp_e<950",
-        "mp_950<e<1100",
-        "mp_e>1100c",
-        "mp_e<950c",
-        "mp_950<e<1100c",
-    ]
+    question_cols = ["mp_e0", "mp_e1", "mp_e2", "mp_e3", "mp_e1c", "mp_e2c", "mp_e3c"]
     return row[question_cols].notna().all(axis=1).all()
 
 
